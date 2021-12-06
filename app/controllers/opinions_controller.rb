aae253bd6f031a70027bc69b313ac9ce59993477
Julien KARMANN <78484043+jkmn-eu@users.noreply.github.com>
2021-12-06 15:12:09 +0100

    the vote page displays the right info when provided with a valid token

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,19 +1,30 @@
 class OpinionsController < ApplicationController
-  before_action :validate_voter, only: :new
 
   def new
     @poll = Poll.find(params[:poll_id])
     @questions = @poll.questions
+    @token = params[:token]
+    @voter = Voter.find_by token: @token
   end
 
   def create
-
+    if allow_vote
+      @opinions = params[:opinions]
+      @opinions.each do |question, answer|
+        Opinion.create(question_id: question.id, answer_id: answer.id, voter_id: @voter.id)
+      end
+      @voter.has_voted == true
+    end
   end
+
+  def invalid_voter
+    redirect_to root_path
+  end
+  helper_method :invalid_voter
 
   private
 
-  def validate_voter
-    @poll = params[:poll_id]
-    @voter = Voter.find_by poll_id: params[:poll_id], token: params[:token]
+  def allow_vote
+    return true if Poll.find(Voter.find(@token)) && @poll.status == "open_for_vote" && @voter.has_voted == false
   end
 end
