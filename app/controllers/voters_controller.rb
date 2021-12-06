b248deee713421a33a4252be34533eec8b5a0fca
Julien KARMANN <78484043+jkmn-eu@users.noreply.github.com>
2021-12-06 15:36:58 +0100

    added helper methods to check the voter's authenticity

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -7,26 +7,32 @@ class VotersController < ApplicationController
     emails_list = params[:voters]
     emails_list.each do |email|
       unless email.empty?
-      voter = Voter.new(poll_id: @poll.id, email: email)
-      voter.regenerate_token
-      voter.save!
-      call_for_votes(email)
+        voter = Voter.new(poll_id: @poll.id, email: email)
+        voter.regenerate_token
+        voter.save!
+        call_for_votes(voter)
       end
     end
     @poll.status = "sent"
+    @poll.save!
     redirect_to poll_path(@poll)
     # AFAIK, the raise showed that the voters were perfectly created with the correct poll_id and the correct email. Yay !
   end
 
-  def call_for_votes(email)
+  call_for_vote_email = "Hi there ! Your input is requested. Please click this link and vote : http://fastdemocracy.herokuapp.com/#{@poll.id}/vote/#{voter.token} ."
+
+
+  def call_for_votes(voter)
     RestClient.post "https://api:#{ENV['MAILGUN_API_KEY']}"\
     "@api.eu.mailgun.net/v3/fastdemocracy.me/messages",
     :from => "FastDemocracy <mailgun@fastdemocracy.me>",
-    :to => "#{email}",
+    :to => "#{voter.email}",
     :subject => "You've been called for a vote",
-    :text => "Hello, this is a test ?"
+    :text => call_for_vote_email
   end
+
 
   # TODO : write a test to get the API response and if positive, go to the next step
   # and change the poll status accordingly.
+
 end
