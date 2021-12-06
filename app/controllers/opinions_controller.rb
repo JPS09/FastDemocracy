a1fda9beed470d1eba02979f7f4efcb087ce41f5
Julien KARMANN <78484043+jkmn-eu@users.noreply.github.com>
2021-12-06 10:52:11 +0100

    created the new route for opinions collection, the token verification to id the voter as legit

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,2 +1,20 @@
 class OpinionsController < ApplicationController
+  before_action :validate_voter, only: :new
+
+  def new
+    @poll = params[:poll_id]
+    @opinion = Opinion.new
+    @questions = @poll.questions
+  end
+
+  def create
+
+  end
+
+  private
+
+  def validate_voter
+    @poll = params[:poll_id]
+    @voter = Voter.find_by poll_id: params[:poll_id], token: params[:token]
+  end
 end
