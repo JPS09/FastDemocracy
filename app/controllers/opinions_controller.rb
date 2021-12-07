d3d71f0025a1dbcaa6a36bb2a329be8d772a32f0
Julien KARMANN <78484043+jkmn-eu@users.noreply.github.com>
2021-12-07 21:28:16 +0100

    adding multiple unstaged changes from other branches. I hate Git.

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -15,6 +15,7 @@ class OpinionsController < ApplicationController
       Opinion.new(question_id: opinion[0], answer_id: opinion[1], voter_id: @voter.id).save!
     end
     @voter.mark_as_has_voted
+    # this function changes the poll status if all voters expressed their vote
     check_if_poll_complete
   end
 
