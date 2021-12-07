462ed4eeb0262b32d3d19897180e3bcb0f55bbe5
Julien KARMANN <78484043+jkmn-eu@users.noreply.github.com>
2021-12-07 17:49:51 +0100

    voting system works

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,12 @@ module ApplicationHelper
     return true if @voter.poll_id == @poll.id
   end
 
+  def check_if_poll_complete
+    total_voters = @poll.voters.count
+    voters_who_voted = Voter.where(poll_id: @poll.id, has_voted: true)
+    if total_voters == voters_who_voted
+      @poll.
+    end
+
+
 end
--- app/models/voter.rb
+++ app/models/voter.rb
@@ -2,4 +2,9 @@ class Voter < ApplicationRecord
   has_secure_token :token
   belongs_to :poll
   has_many :opinions, dependent: :destroy
+
+  def mark_as_has_voted
+    self.has_voted = true
+    save!
+  end
 end
