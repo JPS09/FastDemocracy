cbf55eff1157227c54e9186661bbb1f328a71518
Simon Chretien <simon.chretien62@gmail.com>
2021-12-09 18:45:31 +0100

    stepper added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,8 @@ require("channels")
 // External imports
 import "bootstrap";
 import { newCharts } from "../plugins/_init_chart.js"
+// import Stepper from 'bs-stepper'
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -32,4 +34,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   newCharts();
+
 });
