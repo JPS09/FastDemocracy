79a1be4147524b418df0ff6b9118037e321e5558
matthieu duperrey <matth.duperrey@gmail.com>
2021-12-09 13:10:52 +0100

    modif answer in progress

---
+++ app/javascript/components/add_answer_field.js
@@ -0,0 +1,12 @@
+const addAnswerField = () => {
+  const selectedForm = document.querySelector("#new_question")
+  const addAnswerButton = document.querySelector(".add_answer")
+  const newAnswer = `<div class="form-group string optional question_answers" ><label class="string optional" for="question_answers">Answers</label><input class="form-control string optional" value="" name="answers[]" type="text" id="question_answers"></div>`
+  addAnswerButton.addEventListener('click', (event) => {
+    addAnswerButton.insertAdjacentHTML("beforebegin", newAnswer);
+
+
+    console.log(event.currentTarget);
+  });
+}
+export {addAnswerField}
