80ece33a86c0d50459925060881c4004391b7bfb
Julien KARMANN <78484043+jkmn-eu@users.noreply.github.com>
2021-12-27 14:48:33 +0100

    saving last changes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :poll
-  has_many :answers, dependent: :destroy
   accepts_nested_attributes_for :answers
 
   validates :title, length: { minimum: 2, maximum: 50, message: "Title must be 2 characters long min. and 50 max." }
